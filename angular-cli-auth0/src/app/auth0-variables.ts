109e56be5fb67143fd1b21878d5f4d74adffaff7
Rui Eloi <rui.eloi@nmbrs.com>
2017-06-23 17:32:03 +0100

    auth0 variables

--- angular-cli-auth0/src/app/auth0-variables.ts
+++ angular-cli-auth0/src/app/auth0-variables.ts
@@ -6,8 +6,8 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'StzPmqOA3kpdBysxo4KmErMLNEvXZVnJ',
-  domain: 'ruieloi.eu.auth0.com',
-  callbackURL: 'http://localhost:4200/callback',
+  clientID: {CLIENTID},
+  domain: {DOMAIN},
+  callbackURL: {CALLBACKURL},
   scopes: 'openid profile'
 };
